ce50a3d87962d5dcc55a271d510b97962b3b78f0
Luiz Gustavo Ventura <luiz.ventura@telefoncia.com>
2022-03-23 21:56:19 -0300

    Add third partie dependencies and implementation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Toast from 'react-native-toast-message';
 
 import Tela1 from './src/Tela1';
 import Tela2 from './src/Tela2';
@@ -9,12 +10,15 @@ const Stack = createNativeStackNavigator();
 
 function Myapp() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Tela1" component={Tela1} />
-        <Stack.Screen name="Tela2" component={Tela2} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Tela1" component={Tela1} />
+          <Stack.Screen name="Tela2" component={Tela2} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <Toast />
+    </>
   );
 }
 
--- src/Tela2.js
+++ src/Tela2.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import {ScrollView, Text, Image} from 'react-native';
+import {ScrollView, Text, Image, TouchableOpacity} from 'react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Toast from 'react-native-toast-message';
 
 import {useRoute} from '@react-navigation/native';
 
@@ -8,13 +11,36 @@ const Tela2 = () => {
 
   const data = route.params.data;
 
+  const showCopyMessage = () => {
+    Toast.show({
+      type: 'success',
+      text1: 'Tudo pronto',
+      text2: 'Descrição copiada com sucesso',
+    });
+  };
+
+  const copyToClipboard = stringToCopy => {
+    Clipboard.setString(stringToCopy);
+    showCopyMessage();
+  };
+
   return (
-    <ScrollView>
+    <ScrollView style={{padding: 10}}>
       <Image source={{uri: data.image}} style={{width: 150, height: 300}} />
       <Text>FILME: {data.filme}</Text>
       <Text>ANO: {data.ano}</Text>
       <Text>ESTILO: {data.estilo}</Text>
-      <Text>DESCRIÇÃO: {data.descricao}</Text>
+      <TouchableOpacity
+        onPress={() => copyToClipboard(data.descricao)}
+        style={{flexDirection: 'row', flex: 1}}>
+        <Text style={{width: '90%'}}>DESCRIÇÃO: {data.descricao}</Text>
+        <Icon
+          name="copy"
+          size={20}
+          color="gray"
+          style={{flexDirection: 'row', flex: 1, justifyContent: 'flex-end'}}
+        />
+      </TouchableOpacity>
     </ScrollView>
   );
 };
